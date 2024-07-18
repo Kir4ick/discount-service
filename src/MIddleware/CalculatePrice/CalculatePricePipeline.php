17f38d14e8db3abd16529ce0e7b3847314bce09e
kir4ick <kir4ick@mail.ru>
2024-07-18 17:50:38 +0400

    fix

--- src/MIddleware/CalculatePrice/CalculatePricePipeline.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\MIddleware\CalculatePrice;
-
-class CalculatePricePipeline
-{
-
-}
--- src/MIddleware/CalculatePrice/Data/CalculatePriceActionOutput.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Middleware\CalculatePrice\Data;
-
-class CalculatePriceActionOutput
-{
-
-}
